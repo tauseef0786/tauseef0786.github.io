e95926f2fe0a72020747eb540cf797c58992816d
Md Tauseef Raza <164810977+tauseef0786@users.noreply.github.com>
2025-04-28 12:01:32 +0530

    resume link changes

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
   Contact With Me
 </button>
         <a
-          href="https://drive.google.com/file/d/1u842if2WjlgueymxjW8JhKQnikHvF6rl/view?usp=sharing"
+          href="https://drive.google.com/file/d/1tykaQ6Sb_7EiVKI1Pzm8pBWxA1esj5Gk/view?usp=sharing"
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
           target="_blank"
           rel="noopener noreferrer"
